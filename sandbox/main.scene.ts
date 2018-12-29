f869ede7b4cb67d9db12322ce48a6a77433e8db8
Rasmus Vermeulen <vermeulen.rasmus@gmail.com>
2018-12-29 20:04:49 +0100

    Add first game

---
+++ sandbox/main.scene.ts
@@ -0,0 +1,5 @@
+import { Scene } from 'phaser';
+
+export class MainScene extends Scene {
+  static readonly key = 'main-scenes';
+}
---
+++ sandbox/main.ts
@@ -0,0 +1,22 @@
+import { Game, AUTO } from 'phaser';
+import { MainScene } from './main.scene';
+
+declare const window: Window & { game?: Game };
+
+if (window.game) {
+  window.location.reload();
+}
+
+window.onload = () => {
+  const game = new Game({
+    width: 800,
+    height: 600,
+    type: AUTO,
+    parent: 'game',
+  });
+  window.game = game;
+
+  game.scene.add(MainScene.key, MainScene);
+
+  game.scene.bringToTop(MainScene.key);
+};

a00a4bd214cb6f71bff64883d9d518dccaf61079
Rasmus Vermeulen <vermeulen.rasmus@gmail.com>
2019-01-07 22:48:33 +0100

    Some config stuff, jest typeahead

--- jest.config.js
+++ jest.config.js
@@ -1,15 +1,21 @@
 module.exports = {
   preset: 'ts-jest',
   testEnvironment: 'jsdom',
+  setupFiles: ['jest-canvas-mock'],
   setupTestFrameworkScriptFile: 'jest-extended',
   verbose: true,
   collectCoverage: true,
-  coveragePathIgnorePatterns: ['\.json'],
+  coveragePathIgnorePatterns: ['.json'],
   globals: {
     'ts-jest': {
-      diagnostics: {
-        warnOnly: true,
-      },
+      diagnostics: false,
+      // diagnostics: {
+      //   warnOnly: true,
+      // },
     },
   },
+  watchPlugins: [
+    'jest-watch-typeahead/filename',
+    'jest-watch-typeahead/testname',
+  ],
 };
